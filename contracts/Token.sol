17568fc1571acfcff2b6c1e65d91543ecacc11ee
Patrick <patrickcallah@gmail.com>
2023-10-30 12:57:56 -0500

    4. token balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,10 +8,13 @@ contract Token {
     string public symbol;
     uint256 public decimals = 18;
     uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
     
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
         totalSupply = 1000000 * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -6,11 +6,14 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-    let token
+    let token, accounts, deployer
 
     beforeEach(async () => {
         const Token = await ethers.getContractFactory('Token')
         token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+
+        accounts = await ethers.getSigners();
+        deployer = accounts[0]
     })
 
     describe('Deployment', () => {
@@ -35,6 +38,11 @@ describe('Token', () => {
             expect(await token.totalSupply()).to.equal(totalSupply)
         })
 
+        it('assigns total supply to deployer', async () => {
+            expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+        })
+
+        
     })
 
     
